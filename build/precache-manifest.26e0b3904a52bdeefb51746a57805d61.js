b4aea7b6f32f89ba73b3265d7c5723b7054decd3
Metform <alexandr8cb@gmail.com>
2020-01-10 19:15:00 +0000

    built 2

---
+++ build/precache-manifest.26e0b3904a52bdeefb51746a57805d61.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6130ade08818526534c",
+    "url": "/static/css/main.661e0f37.chunk.css"
+  },
+  {
+    "revision": "d6130ade08818526534c",
+    "url": "/static/js/main.d6130ade.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f7bade3a6ce3024b9983",
+    "url": "/static/css/2.e9de5eca.chunk.css"
+  },
+  {
+    "revision": "f7bade3a6ce3024b9983",
+    "url": "/static/js/2.f7bade3a.chunk.js"
+  },
+  {
+    "revision": "5221c1ff101baaaf5beb5cbfc382c65c",
+    "url": "/static/media/initPointIcon.5221c1ff.svg"
+  },
+  {
+    "revision": "7c9810df320391e9552c48c3e7a3ac6d",
+    "url": "/static/media/pointIcon.7c9810df.svg"
+  },
+  {
+    "revision": "c2210c6f21604038c6438e1cfeea9f9e",
+    "url": "/static/media/t2.c2210c6f.jpg"
+  },
+  {
+    "revision": "f06e4f9376dce5c5c73c6424d2fcab51",
+    "url": "/static/media/t1.f06e4f93.jpg"
+  },
+  {
+    "revision": "95688681d6cefdb6b53943d298d1f2a8",
+    "url": "/static/media/t3.95688681.jpg"
+  },
+  {
+    "revision": "8ff32abcb42aa0b54318daa28c786594",
+    "url": "/static/media/background.8ff32abc.jpg"
+  },
+  {
+    "revision": "0ab0d324af2bd6ec54fbbb03c5c7002f",
+    "url": "/index.html"
+  }
+];
--- src/_config.js
+++
@@ -1,7 +0,0 @@
-module.exports.config = {
-    auth0: {
-        domain: 'turkey-tours.auth0.com',
-        clientId: 'uCDJ1QFHxtBHcDfE105TtXpEBt8pRdsl',
-        callbackPath: '/callback'
-    }
-}
